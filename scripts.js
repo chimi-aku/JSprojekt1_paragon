aed2361ce4894984ca7a24d0c97f02267eb56709
Łukasz Terpiłowski <lukaszterpilowski9@gmail.com>
2021-11-21 14:48:09 +0100

    displaying and adding products, no styles

---
+++ scripts.js
@@ -0,0 +1,124 @@
+const addSection = document.querySelector('.add_section');
+const addProductBtn = addSection.querySelector('.add_product_btn');
+addProductBtn.addEventListener('click', addProduct);
+
+
+class Product {
+    constructor(name, price, amount){
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.sum = price * amount;
+    }
+}
+
+let productList = [];
+
+function showProducts() {
+    const paragon = document.querySelector('.paragon');
+    paragon.innerHTML = '';
+
+    
+    const list = document.createElement('ul');
+    paragon.appendChild(list);
+
+    /* displaying labels */
+
+    const productLabels = document.createElement('li');
+    productLabels.classList.add('product');
+    productLabels.classList.add('labels');
+
+
+    const labels = ['number', 'name', 'price', 'amount', 'sum'];
+    for(l of labels) {
+        const p = document.createElement('p');
+        p.textContent = l;
+        p.classList.add('pruduct_label');
+        productLabels.appendChild(p);
+    }
+
+    paragon.appendChild(productLabels);
+
+
+    /* displaying products */
+
+    console.log(productList);
+
+    for(const product of productList){
+        // productElement - product li HTML element
+        // product object of Product class cointaing data
+        let i = 1;
+        console.log(product);
+
+        const productElement = document.createElement('li');
+        productElement.classList.add('product');
+
+        const productNumber = document.createElement('p');
+        productNumber.textContent = i;
+        productElement.appendChild(productNumber);
+
+        for(prop in product) {
+            const p = document.createElement('p');
+            p.textContent = product[prop];
+            p.classList.add('product_property');
+            productElement.appendChild(p);
+        }
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete_btn');
+        productElement.appendChild(deleteBtn);
+
+        paragon.appendChild(productElement);
+
+        i++;
+
+    }
+
+}
+
+function addProduct() {
+    console.log('add p');
+
+    const inputProductName = addSection.querySelector('#add_product_name');
+    const inputProductPrice = addSection.querySelector('#add_product_price');
+    const inputProductAmount = addSection.querySelector('#add_product_amount');
+
+    const name = inputProductName.value;
+    const price = inputProductPrice.value;
+    const amount = inputProductAmount.value;
+
+    const newProduct = new Product(name, price, amount);
+    productList.push(newProduct);
+
+    
+
+    showProducts();
+
+    inputProductName.value = '';
+    inputProductPrice.value = '';
+    inputProductAmount.value = '';
+
+    // Local storage to do
+
+
+}
+
+
+
+/** Local storage **/
+
+function updateLocalStorage() {
+    if(typeof localStorage !== 'undefined'){
+        localStorage.removeItem('product list');
+        localStorage.setItem('product list', JSON.stringify(productList));
+    }
+}
+
+function loadDataFromLocalStorage() {
+    if(typeof localStorage !== 'undefined' && localStorage.getItem('product list') != 'undefined'){
+        productList = JSON.parse(localStorage.getItem('product list'));
+    }
+}
+
+/** MAIN **/
+
